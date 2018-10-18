498326cc385eb3a7d94b6963909bfb85c1fa0300
IcaroCSQ <icaro_carlos_@hotmail.com>
2018-10-18 10:59:19 -0300

    Mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,10 @@ package application;
  * @author IcaroCarlos
  */
 public class Program {
-    
+
     public static void main(String[] args) {
-        
         System.out.println("Bom dia");
         System.out.println("Boa tarde!");
+        System.out.println("Boa noite!");
     }
-    
 }
